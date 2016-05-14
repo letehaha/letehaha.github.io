0a5a3cefe944e26270baa84a9bb0c9e55f2b166b
dmitry <letehaha@gmail.com>
2016-05-14 14:29:06 +0300

    Atlantida

---
+++ Atlantida/js/common.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	// Instantiate MixItUp:
+
+	$('#wrapper_portfolio_elem').mixItUp();
+
+
+	$("a[href*='#']").mPageScroll2id({
+		scrollEasing: "easeInOutQuart",
+		scrollSpeed: 900
+	});
+});
+
