f14de892d37a0bfd4816e135a6948ff261926990
dmitry <letehaha@gmail.com>
2016-05-24 01:38:04 +0300

    update

--- MoGo/js/common.js
+++ MoGo/js/common.js
@@ -11,12 +11,12 @@ $(function() {
 		}
 	});
  	
- 	$("a[href*='#']").mPageScroll2id();
- 	
-	// $(".services__sect-content_accordeon_item p").not(':first').hide();
-	// $(".services__sect-content_accordeon_item p").prev().click(function() {
-	// 	$(".services__sect-content_accordeon_item p").not(this).slideUp();
-	// 	$(this).next().not(":visible").slideDown();
-	// });
-// $(".services__sect-content_accordeon_item h3 span").css('transform','rotate(180deg)')
+
+ 	$("a[href*='#']").smoothScroll({speed: 1000});
+	
+ 	$(".first_slide_slider").slick({
+ 		dots: true,
+		speed: 500
+ 	});
+
 });
