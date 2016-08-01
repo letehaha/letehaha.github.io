ff87ff895af18e5fc22ae121438e45eaeea574c6
Dmitry Sviridenko <letehaha@gmail.com>
2016-08-01 16:25:05 +0300

    update

--- touch_slider/js/common.js
+++ touch_slider/js/common.js
@@ -1,88 +1,100 @@
 //начальная позиция
 var pos = 0;
 //номер слайда
-var totalSlides = $('#slider-wrap ul li').length;
+var totalSlides = document.getElementsByClassName('slide').length;
 //получить ширину слайда
-var sliderWidth = $('#slider-wrap').width();
-
+var sliderWidth = document.getElementById('slider-wrap').offsetWidth;
+var sliderLine = document.getElementById('slider');
 function slideLeft(){
 	pos--;
 	if(pos==-1){ pos = totalSlides-1; }
-	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 	
+	$('#slider').css('left', -(sliderWidth*pos));
+	//sliderLine.style.left = -(sliderWidth*pos);
 
 	countSlides();
 	pagination();
-}
+};
 
 function slideRight(){
 	pos++;
-	if(pos==totalSlides){ pos = 0; }
-	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
+	if(pos==totalSlides){ pos = 0;}
+	$('#slider').css('left', -(sliderWidth*pos)); 
+	//sliderLine.style.left = -(sliderWidth*pos);
 
 	countSlides();
 	pagination();
-}
+};
 
 function countSlides(){
+	// document.getElementById('counter').document.body(pos+1 + ' / ' + totalSlides);
 	$('#counter').html(pos+1 + ' / ' + totalSlides);
-}
+};
 countSlides();
 
 
 function pagination(){
 	$('#pagination-wrap ul li').removeClass('active');
 	$('#pagination-wrap ul li:eq('+pos+')').addClass('active');
-}
+};
 pagination();
-	
+
 
 
 $(document).ready(function(){
 	
 	$.mobile.loading().hide();
 	//ширина слайдера зависит от количества слайдов
-	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+
+	// sliderLine.style.width = sliderWidth*totalSlides
+	$('#slider').width(sliderWidth*totalSlides);
 	
-    //следющий слайд	
-	$('#next').click(function(){
-		slideRight();
-	});
-	
+	//следующий слайд
+	document.getElementById('next').onclick = slideRight;
 	//предыдущий слайд
-	$('#previous').click(function(){
-		slideLeft();
-	});
+	document.getElementById('previous').onclick = slideLeft;
 	
 	//переключение слайдов по стрелкам
-	$(document).on('keydown', function(e) {
+	$(document).on('keydown', function(e){
 		if (e.keyCode === 37 || e.keyCode === 40)  slideLeft();
 		else if (e.keyCode === 39 || e.keyCode === 38) slideRight();
 	});
 
-	$('#slider-wrap ul li').on('swipeleft', function(event) {
-		slideRight();
-	});
 
-	$('#slider-wrap ul li').on('swiperight', function(event) {
-		slideLeft();
-	});
+	//только для тач девайсов
+	function is_touch_device(){
+		return (('ontouchstart' in window)
+			|| (navigator.MaxTouchPoints > 0)
+			|| (navigator.msMaxTouchPoints > 0)); //если количество тач касаний > 0, то экран сенсорный
+	};
 
+
+    if (is_touch_device()){
+		$('#slider-wrap ul li').on('swipeleft', function(e) {
+			slideRight();
+		});
+		$('#slider-wrap ul li').on('swiperight', function(e) {
+			slideLeft();
+		});
+	} else{
+		false
+	};
 
 	//автоматическая прокрутка
 	// var autoSlider = setInterval(slideRight, 3000);
 	
 	//создание пагинации для каждого слайда
-	$.each($('#slider-wrap ul li'), function() {
+	$.each($('#slider li'), function(){
 	   var li = document.createElement('li');
-	   $('#pagination-wrap ul').append(li);	   
+	   // document.getElementById('pagination-wrap').firstChild.className = 'active';
+	   $('#pagination-wrap ul').append(li); 
 	});
 
 	
 	//показать/спрятать кнопки когда hover
 	//пауза автоматической прокрутки когда hover
 	$('#slider-wrap').hover(
-	  function(){ $(this).addClass('active'); clearInterval(autoSlider); }, 
-	  function(){ $(this).removeClass('active'); autoSlider = setInterval(slideRight, 3000); }
+	  function(){ $(this).addClass('active');}, //clearInterval(autoSlider); 
+	  function(){ $(this).removeClass('active');} //autoSlider = setInterval(slideRight, 3000);
 	);
 	
 });
