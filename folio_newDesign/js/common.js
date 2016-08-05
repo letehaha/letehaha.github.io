78c6ecd2af5a4946eee5a956c724ef200725c80f
letehaha <letehaha@gmail.com>
2016-08-05 22:45:49 +0300

    update

--- folio_newDesign/js/common.js
+++ folio_newDesign/js/common.js
@@ -1,26 +1,26 @@
-$(function() {
-	$('.folio').click(function(e) {
-		$(this).css({"transform":"translate(0)"});
-		$('.exp').css({"transform":"translateX(115%)"});
-		$('.contact').css({"transform":"translateY(105%)"});
-	});
+// $(function() {
+// 	$('.folio').click(function(e) {
+// 		$(this).css({"transform":"translate(0)"});
+// 		$('.exp').css({"transform":"translateX(115%)"});
+// 		$('.contact').css({"transform":"translateY(105%)"});
+// 	});
 
-	$('.exp').click(function(e) {
-		$(this).css({"transform":"translate(0)"});
-		$('.folio').css({"transform":"translateY(-105%)"});
-		$('.contact').css({"transform":"translateY(105%)"});
-	});
+// 	$('.exp').click(function(e) {
+// 		$(this).css({"transform":"translate(0)"});
+// 		$('.folio').css({"transform":"translateY(-105%)"});
+// 		$('.contact').css({"transform":"translateY(105%)"});
+// 	});
 
-	$('.contact').click(function(e) {
-		$(this).css({"transform":"translate(0)"});
-		$('.exp').css({"transform":"translateX(105%)"});
-		$('.folio').css({"transform":"translateY(-105%)"});
-	});
+// 	$('.contact').click(function(e) {
+// 		$(this).css({"transform":"translate(0)"});
+// 		$('.exp').css({"transform":"translateX(105%)"});
+// 		$('.folio').css({"transform":"translateY(-105%)"});
+// 	});
 
-	$('.arrow').click(function(e) {
-		e.stopPropagation();
-		$('.exp').css({"transform":"translateX(105%)"});
-		$('.folio').css({"transform":"translateY(-95%)"});
-		$('.contact').css({"transform":"translateY(95%)"});
-	});
-});
+// 	$('.arrow').click(function(e) {
+// 		e.stopPropagation();
+// 		$('.exp').css({"transform":"translateX(105%)"});
+// 		$('.folio').css({"transform":"translateY(-95%)"});
+// 		$('.contact').css({"transform":"translateY(95%)"});
+// 	});
+// });
