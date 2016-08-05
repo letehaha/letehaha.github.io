2c7973d67687b5ac0a500c71939a79c46693aac0
letehaha <letehaha@gmail.com>
2016-08-05 21:14:22 +0300

    update

--- folio_newDesign/js/common.js
+++ folio_newDesign/js/common.js
@@ -1,19 +1,26 @@
 $(function() {
 	$('.folio').click(function(e) {
-		$(this).css({"transform":"translateY(0)"});
+		$(this).css({"transform":"translate(0)"});
 		$('.exp').css({"transform":"translateX(115%)"});
 		$('.contact').css({"transform":"translateY(105%)"});
 	});
 
 	$('.exp').click(function(e) {
-		$(this).css({"transform":"translateX(0)"});
+		$(this).css({"transform":"translate(0)"});
 		$('.folio').css({"transform":"translateY(-105%)"});
 		$('.contact').css({"transform":"translateY(105%)"});
 	});
 
 	$('.contact').click(function(e) {
-		$(this).css({"transform":"translateY(0)"});
-		$('.exp').css({"transform":"translateX(115%)"});
+		$(this).css({"transform":"translate(0)"});
+		$('.exp').css({"transform":"translateX(105%)"});
 		$('.folio').css({"transform":"translateY(-105%)"});
+	});
+
+	$('.arrow').click(function(e) {
+		e.stopPropagation();
+		$('.exp').css({"transform":"translateX(105%)"});
+		$('.folio').css({"transform":"translateY(-95%)"});
+		$('.contact').css({"transform":"translateY(95%)"});
 	});
 });
