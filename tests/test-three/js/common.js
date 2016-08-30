a83e15f15ff9ad8d01ea1de4e8e648d0ac5cefe0
letehaha <letehaha@gmail.com>
2016-08-30 20:16:12 +0300

    update

---
+++ tests/test-three/js/common.js
@@ -0,0 +1,10 @@
+$(function() {
+ 	$(".burger-menu").click(function (e) {
+		$(this).toggleClass("menu-on");
+		$('.burger-list').fadeToggle('burger-list-active');
+		$('.burger_elem').click(function(e) {
+			$(".burger-menu").toggleClass("menu-on");
+			$('.burger-list').fadeToggle('burger-list-active');
+		});
+	});
+});
