2c9b53b5251474f29293b8105bcc02f1f6b46793
letehaha <letehaha@gmail.com>
2016-09-18 10:29:27 +0300

    update

--- Atlantida/js/common.js
+++ Atlantida/js/common.js
@@ -1,13 +1,15 @@
 $(function(){
 
-	// Instantiate MixItUp:
-
-	$('#wrapper_portfolio_elem').mixItUp();
-
+	$('#wrapper-portfolio-elem').mixItUp();
 
 	$("a[href*='#']").mPageScroll2id({
 		scrollEasing: "easeInOutQuart",
 		scrollSpeed: 900
 	});
-});
 
+	$('.testimonials-slider').slick({
+  	 dots: true,
+  	 arrows: false,
+  	 infinite: true
+	});
+});
